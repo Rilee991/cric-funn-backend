d128b44db6a5d9d995d2c8a50aad7196dcd4940c
cypher33 <rohitkumar25.rkl@gmail.com>
2023-03-28 22:51:59 +0530

    added cron job for odds

--- app.js
+++ app.js
@@ -1,1 +1,67 @@
-console.log("date is:", new Date());
+const cron = require('node-cron');
+const admin = require('firebase-admin');
+const moment = require('moment');
+const axios = require('axios');
+const { get } = require('lodash');
+require('dotenv').config();
+
+
+const { firebaseConfig } = require('./config');
+
+const updateOddsForIpl = async () => {
+    try {
+        console.log(`Inside updateOddsForIpl: start - ${new Date()}`);
+        const db = admin.firestore();
+        const tomorrowStartDate = moment().add(1,"day").startOf("day").toISOString();
+        const tomorrowEndDate = moment().add(1,"day").endOf("day").toISOString();
+        console.log(`startdate: ${tomorrowStartDate}, enddate: ${tomorrowEndDate}`);
+
+        console.log(`fetching matches`);
+        const resp = await db.collection('ipl_matches').where("dateTimeGMT", ">=", tomorrowStartDate).where("dateTimeGMT", "<=", tomorrowEndDate).get();
+        const matches = resp.docs.map(doc => doc.data());
+        console.log(`${matches.length} matches found`);
+
+        console.log('fetching odds');
+        const upcomingOdds = await getUpcomingOdds();
+        console.log('fetching odds completed');
+        
+        for(const match of matches) {
+            const matchOddsVal = upcomingOdds.filter(uo => uo.commence_time == match.dateTimeGMT) || {};
+            const outcomes = get(matchOddsVal, '[0].bookmakers[0].markets[0].outcomes', []);
+            console.log(match.name, outcomes);
+
+            await db.collection('ipl_matches').doc(match.id).update({
+                odds: outcomes
+            });
+        }
+        console.log(`Updation completed for ${new Date()}`);
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+const getUpcomingOdds = async () => {
+    const config = {
+        method: 'get',
+        url: 'https://api.the-odds-api.com/v4/sports/cricket_ipl/odds/?apiKey=f8330878dd0337f2838d2493f69bff14&regions=uk&dateFormat=iso',
+        headers: { }
+    };
+
+    const resp = await axios(config);
+    const oddsResp = get(resp, 'data', []);
+
+    return oddsResp;
+}
+
+const start = () => {
+    console.log(`Cron Job Initializing`);
+    admin.initializeApp({ credential: admin.credential.cert(firebaseConfig) });
+    console.log(process.env.NODE_ENV);
+
+    const timer = "0 5 * * *";
+    console.log(`Timer: ${timer}`);
+
+    cron.schedule(timer, updateOddsForIpl);
+}
+
+start();
---
+++ config.js
@@ -0,0 +1,16 @@
+const firebaseConfig = {
+    "type": "service_account",
+    "project_id": "cric-funn",
+    "private_key_id": "866bbb52b42161830304da7147f7b5efcb3d9172",
+    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQC6Q4Y4Mb+k0PhT\nuowq6Yks3XuEPWGOhg4Hs42GijykxCjWXFRwzFi82mcCNaxAPhK3HkC9Gje/EU4T\nRrtJdqW80fnAorO3elD9c+U3Ae0iBumlJouifBXTg6KPxgMzfGiqhYLU+iIxYwKB\nwn/62UZXVNFZ6Vzckdb2h5GY1ueute7aUjCl2s83e0d471o5Pbhl3D6GWSUgK0/H\nAmrBKNngO+xLB/FJGhnDBLIrjJNG/nS8i1vSfHJcr1d/pPaVnSeF3coRwqZv33q2\n8y2UWo8xIw1ixCnmrwzgOb9upmXmhHfIpsYSADbpdvLbA50B12ng2X+o+GetQnYW\nGS/A56vpAgMBAAECggEAQMDbxuponQQ26GjIdNymXy4KVDFAmps5Am1l7JlfNsqp\nBcioZGRuDw2AVRDStqy9teJzECquqaJ6+ZGT55izSp7WzCTkBIul307sp0Bor3Um\nwc7SKWUo81TmbUQh9cKw4mMy1ha1T2UDyJeVBWCGpzKp6y+JJHcg+6iqsa84cRBf\ni3TgyougGvoZI2dl67rBbuZd7sVY9/7dXq4d+GpP03IODqoZc2NRJhzJ8Xgzson2\nh+RyZXq4OQITGAzrVWolewtkkcqKx6uSXukakRlXUkR4I6fgbmbMF+YjiEH9Pm/p\npIegCMDHK2HA8eMYwCbOu7V5u2whJVAVaI7gyoUIPwKBgQDnJTtPeUvdfnGwyuXt\nRPAoA3BHsJu6nIES/KPid/Ur2qZv4KstSa5SccP0UO9cSjlCcXak/GEtFY7RAyD6\nUIaUw2ONLlWIkAXjLtuy3sD76XQxOZcUJEXGDlDrPWXaXawXRUY7C6W+2mWjvBLi\njzeSBPvgoyzNOt2l4HgaJqMbmwKBgQDOStRY5Yp4dHdb/628C2Qyj3hMPV+1hfnU\notLXm4gvPajXH3RX7weuMxzL37PWRlIXjvE0XjcHAobV3mZLiZtUhpzIQpnqJWuq\n7l0pMD57/HQSwCJhrHbgzHUTBVF+rmDESSMKfsptZ7dr4Nqv3EcZ2VAQA9jvLgCI\niaw87jLYywKBgQDNt1Yk8igx9fILTVj7JWqzwfOw6h5/JADAPDdDPE3ofM26LnNK\neBP8biHfFBUjxy9U0hSs261E6bXUkXtNLFPKnGbCZOVdCAklWzPO6u9IhEqT8NRj\n6bBrh5w+8MLwaIPbphzOOhyGUms5IQ6RsrLbczIUiEU8VlKj6bYaWB68VwKBgDJ+\n6p+gAFcf2x+v5C0FdM9TgxTWaRkb5F2CRa31Kk61/yA5WNc1cWHYrRHSfkjTCcEu\ngihZb4rNuYecWtneoyD7mmECvH4uDr7WfgNZBdJ/p0H3ufLCro60PWwcfyhNMu6Z\nIHSvOImzO5iyf5VQjC7tnMVuy6kwT30chgacH+IlAoGBAIKoN8Vs1iAE/gsYmVtz\nf9ze6CyCXvPkk34a7QrMclr3F4R+piEjZVzvagiraXXmEWV1qzjnmaI9xVPAiutN\nBxS2yTuT/p3Say0cSi9D9M6wa3DdbQLgyuCiOJ1ztu+Zk5XWShSAMWtktsJ6EYI5\n1dOl+fpivzhuWGs/c1A0eNjU\n-----END PRIVATE KEY-----\n",
+    "client_email": "firebase-adminsdk-6clk9@cric-funn.iam.gserviceaccount.com",
+    "client_id": "114522927589627899907",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-6clk9%40cric-funn.iam.gserviceaccount.com"
+};
+
+module.exports = {
+    firebaseConfig
+}
