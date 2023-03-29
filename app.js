b5dcec155a0301166be6c2ad9f1612791956a13e
cypher33 <rohitkumar25.rkl@gmail.com>
2023-03-29 13:26:40 +0530

    updated code to update odds

--- app.js
+++ app.js
@@ -6,75 +6,59 @@ const { get } = require('lodash');
 require('dotenv').config();
 
 
-// const { firebaseConfig } = require('./config');
+const { firebaseConfig } = require('./config');
 
-// const updateOddsForIpl = async () => {
-//     try {
-//         console.log(`Inside updateOddsForIpl: start - ${new Date()}`);
-//         const db = admin.firestore();
-//         const tomorrowStartDate = moment().add(1,"day").startOf("day").toISOString();
-//         const tomorrowEndDate = moment().add(1,"day").endOf("day").toISOString();
-//         console.log(`startdate: ${tomorrowStartDate}, enddate: ${tomorrowEndDate}`);
+const updateOddsForIpl = async () => {
+    try {
+        console.log(`Inside updateOddsForIpl: start - ${new Date()}`);
+        const db = admin.firestore();
+        const tomorrowStartDate = moment().add(1,"day").startOf("day").toISOString();
+        const tomorrowEndDate = moment().add(1,"day").endOf("day").toISOString();
+        console.log(`startdate: ${tomorrowStartDate}, enddate: ${tomorrowEndDate}`);
 
-//         console.log(`fetching matches`);
-//         const resp = await db.collection('ipl_matches').where("dateTimeGMT", ">=", tomorrowStartDate).where("dateTimeGMT", "<=", tomorrowEndDate).get();
-//         const matches = resp.docs.map(doc => doc.data());
-//         console.log(`${matches.length} matches found`);
+        console.log(`fetching matches`);
+        const resp = await db.collection('ipl_matches').where("dateTimeGMT", ">=", tomorrowStartDate).where("dateTimeGMT", "<=", tomorrowEndDate).get();
+        const matches = resp.docs.map(doc => doc.data());
+        console.log(`${matches.length} matches found`);
 
-//         console.log('fetching odds');
-//         const upcomingOdds = await getUpcomingOdds();
-//         console.log('fetching odds completed');
+        console.log('fetching odds');
+        const upcomingOdds = await getUpcomingOdds();
+        console.log('fetching odds completed');
         
-//         for(const match of matches) {
-//             const matchOddsVal = upcomingOdds.filter(uo => uo.commence_time == match.dateTimeGMT) || {};
-//             const outcomes = get(matchOddsVal, '[0].bookmakers[0].markets[0].outcomes', []);
-//             console.log(match.name, outcomes);
+        for(const match of matches) {
+            const matchOddsVal = upcomingOdds.filter(uo => uo.commence_time == match.dateTimeGMT) || {};
+            const outcomes = get(matchOddsVal, '[0].bookmakers[0].markets[0].outcomes', []);
+            console.log(match.name, outcomes);
 
-//             await db.collection('ipl_matches').doc(match.id).update({
-//                 odds: outcomes
-//             });
-//         }
-//         console.log(`Updation completed for ${new Date()}`);
-//     } catch (e) {
-//         console.log(e);
-//     }
-// }
+            await db.collection('ipl_matches').doc(match.id).update({
+                odds: outcomes
+            });
+        }
+        console.log(`Updation completed for ${new Date()}`);
+    } catch (e) {
+        console.log(e);
+    }
+}
 
-// const getUpcomingOdds = async () => {
-//     const config = {
-//         method: 'get',
-//         url: 'https://api.the-odds-api.com/v4/sports/cricket_ipl/odds/?apiKey=f8330878dd0337f2838d2493f69bff14&regions=uk&dateFormat=iso',
-//         headers: { }
-//     };
+const getUpcomingOdds = async () => {
+    const config = {
+        method: 'get',
+        url: 'https://api.the-odds-api.com/v4/sports/cricket_ipl/odds/?apiKey=f8330878dd0337f2838d2493f69bff14&regions=uk&dateFormat=iso',
+        headers: { }
+    };
 
-//     const resp = await axios(config);
-//     const oddsResp = get(resp, 'data', []);
+    const resp = await axios(config);
+    const oddsResp = get(resp, 'data', []);
 
-//     return oddsResp;
-// }
+    return oddsResp;
+}
 
-const start = () => {
-    console.log(`Cron Job Initializing`);
-    // admin.initializeApp({ credential: admin.credential.cert(firebaseConfig) });
-    console.log(process.env.NODE_ENV);
-
-    // const timer = "0 5 * * *";
-    // console.log(`Timer: ${timer}`);
-
-    // cron.schedule(timer, updateOddsForIpl);
-
-//     on: push
-// jobs:
-//   build-node:
-//     runs-on: ubuntu-latest
-//     container: node:16
-//     steps:
-//       - run: node --version
-//       - run: npm --version
-//       - uses: actions/checkout@v3
-//       - run: ls
-//       - run: npm install
-//       - run: node app.js
+const start = async () => {
+    console.log(`Job started at ${new Date()}`);
+    admin.initializeApp({ credential: admin.credential.cert(firebaseConfig) });
+    console.log(`Env Vars: ${process.env.NODE_ENV}`);
+    await updateOddsForIpl();
+    console.log(`Job ended at ${new Date()}`);
 }
 
 start();
